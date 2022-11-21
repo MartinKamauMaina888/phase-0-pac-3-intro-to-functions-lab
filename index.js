ffd62a9a222db4b1c4ba3fecef79aa9660a74887
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-11-21 17:31:03 +0300

    Completed assignnment

---
+++ index.js
@@ -0,0 +1,64 @@
+function shout(string) {
+    return string;
+}
+"Hello!".toUpperCase();
+
+function shout(string){
+return string.toUpperCase();
+}
+
+function whisper(string){
+    return string;
+}
+ "HELLO!".toLowerCase();
+
+ function whisper(string){
+    return string.toLowerCase();
+ }
+
+//logShout//
+
+function logShout(string)
+{
+    console.log(string);
+}
+"Hello!".toUpperCase();
+
+function logShout(string){
+
+    console.log(string.toUpperCase());
+
+}
+
+//logWhisper//
+function logWhisper(string){
+
+    console.log(string)
+}
+
+"HELLO!".toLowerCase();
+
+function logWhisper(string){
+
+    console.log(string.toLowerCase());
+
+}
+//sayHiToHeadphonedRoommate//
+function sayHiToHeadphonedRoommate(string)
+{
+let inaudible = "I can't hear you!";
+let audible = "YES INDEED!";
+let love = "I would love to!";
+if (string.toLowerCase(string) === string)
+{
+return inaudible
+}
+else if (string.toUpperCase(string) === string)
+{
+return audible
+}
+else if ("Let's have dinner together!" === string)
+{
+return love
+}
+}
